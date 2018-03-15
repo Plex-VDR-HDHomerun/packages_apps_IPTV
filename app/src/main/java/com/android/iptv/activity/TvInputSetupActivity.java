4465e5e235c712847647a370f0947c971d60085b
Matthew J. Gorski <matt.gorski@gmail.com>
2018-03-15 16:39:13 -0400

    initial commit rebrand to com.android.iptv

---
+++ app/src/main/java/com/android/iptv/activity/TvInputSetupActivity.java
@@ -0,0 +1,18 @@
+package com.android.iptv.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.android.iptv.R;
+
+/**
+ * Created by notz.
+ */
+public class TvInputSetupActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.setup_activity);
+    }
+}
